a71f385712d9fabacfe13732c9300ddd9db4079d
jayeshsingh9767 <jayeshsingh9767@gmail.com>
2018-12-28 10:30:55 +0530

    Like button workig with AJAX call but don't support redundent like and dislikes

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import loader, HttpResponse, get_object_or_404, HttpResponseRedirect
 from .models import Posts
 from django.template.loader import render_to_string
+from django.http import JsonResponse
 from signup.models import Profile
 
 
@@ -14,8 +15,14 @@ def home(request):
 
 
 def like_post(request):
+    all_posts = Posts.objects.all()
     print("Insisde Like Post")
+    print('ID coming from form is', request.POST.get('id'))
     post = get_object_or_404(Posts, id=request.POST.get('id'))  # for AJAX call
+    context = {
+        'all_posts': all_posts,
+        'post': post
+    }
     if post.likes.filter(id=request.user.id).exists():
         post.likes.remove(request.user)                 # Liking The Post
         print("DisLiking the post")
@@ -23,6 +30,6 @@ def like_post(request):
         post.likes.add(request.user)
         print("Liking the post")
     if request.is_ajax():
-        html = render_to_string('')
-
-    return HttpResponseRedirect('/')
+        print('Hey its an AJAX calls')    # TEsting AJAX request
+        html = render_to_string('like_section.html', context, request=request)
+        return JsonResponse({'form': html})
