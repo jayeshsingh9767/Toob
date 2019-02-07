3c84d76cb06e5fbe158d220ad4228145a0c25f29
jayeshsingh9767 <jayeshsingh9767@gmail.com>
2019-02-07 21:37:18 +0530

    Added followers and followings page

--- explore/urls.py
+++ explore/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.explore, name='explore'),
-    
+    path('followers/', views.followers, name='followers'),
+    path('followings/', views.followings, name='followings')
 ]
--- explore/views.py
+++ explore/views.py
@@ -6,17 +6,30 @@ from signup.models import Profile
 
 
 def explore(request):
-    if request.user.is_authenticated:
-        all_user = Profile.objects.all().exclude(user=request.user).order_by('-level')
-        logged_in_user_profile = Profile.objects.get(user=request.user)
-        followings = Profile.objects.filter(user=request.user).values('follows')
-        followers = Profile.objects.filter(follows=logged_in_user_profile)
-        context = {
-            'all_user': all_user,
-            'logged_in_user_profile': logged_in_user_profile,
-            'followings': followings,
-            'followers': followers,
-        }
-        return render(request, 'explore.html', context)
-    else:
-        return HttpResponseRedirect(reverse("login"))
+    all_user = Profile.objects.all().exclude(user=request.user).order_by('-level')
+    logged_in_user_profile = Profile.objects.get(user=request.user)
+    context = {
+        'all_user': all_user,
+        'logged_in_user_profile': logged_in_user_profile,
+    }
+    return render(request, 'explore.html', context)
+
+
+def followers(request):
+    logged_in_user_profile = Profile.objects.get(user=request.user)
+    followers = Profile.objects.filter(follows=logged_in_user_profile)
+    context = {
+        'logged_in_user_profile': logged_in_user_profile,
+        'followers': followers,
+    }
+    return render(request, 'followers.html', context)
+
+
+def followings(request):
+    logged_in_user_profile = Profile.objects.get(user=request.user)
+    followings = Profile.objects.get(id=2).follows.all()
+    context = {
+        'logged_in_user_profile': logged_in_user_profile,
+        'followings': followings,
+    }
+    return render(request, 'followings.html', context)
