dca6c1fa03148955442384f3dee6e7f4680e2286
jayeshsingh9767 <jayeshsingh9767@gmail.com>
2019-02-12 18:09:20 +0530

    Solved bugs on Comments mdule

--- details/views.py
+++ details/views.py
@@ -20,9 +20,10 @@ def details_post(request, post_id):
     print("Inside Details view")
     post = get_object_or_404(Posts, id=post_id)
     comments = Comment.objects.all().filter(post=post_id)
-    if not (post.views.filter(id=request.user.id).exists()):
-        post.views.add(request.user)                # User is Viewing the post
-        update_trending_ratio(post, comments)
+    if request.user.is_authenticated:
+        if not (post.views.filter(id=request.user.id).exists()):
+            post.views.add(request.user)                # User is Viewing the post
+            update_trending_ratio(post, comments)
     share_string = quote_plus(post.title)
     trending = Posts.objects.exclude(id=post_id).order_by("-trending_ratio")
     also_like = Posts.objects.exclude(id=post_id).filter(reduce(operator.or_, (Q(tags__contains=x) for x in post.tags.split())))
@@ -39,18 +40,21 @@ def details_post(request, post_id):
 
 
 def comment_submit(request, post_id):
-    print('Inside Comment Submit')
-    if request.method == 'POST':
-        post = Posts.objects.get(id=post_id)
-        comments = Comment.objects.all().filter(post=post)
-        owner = Profile.objects.get(user=request.user)
-        content = request.POST.get("comment")
-        print('Content is', content)
-        if post.user_profile != owner:
-            if not comments.filter(user=request.user.id).exists():
-                update_trending_ratio(post, comments)
-        Comment.objects.create(comment=content, post=post,
-                               user=request.user)
-        print(' Comment is ', content)
-        return HttpResponseRedirect(reverse('details_post',
-                                    kwargs={'post_id': int(post_id)}))
+    if request.user.is_authenticated:
+        print('Inside Comment Submit')
+        if request.method == 'POST':
+            post = Posts.objects.get(id=post_id)
+            comments = Comment.objects.all().filter(post=post)
+            owner = Profile.objects.get(user=request.user)
+            content = request.POST.get("comment")
+            print('Content is', content)
+            if post.user_profile != owner:
+                if not comments.filter(user=request.user.id).exists():
+                    update_trending_ratio(post, comments)
+            Comment.objects.create(comment=content, post=post,
+                                   user=request.user)
+            print(' Comment is ', content)
+            return HttpResponseRedirect(reverse('details_post',
+                                        kwargs={'post_id': int(post_id)}))
+    else:
+        return HttpResponseRedirect(reverse('login'))
